c9c0b4db7d9cbeb34dc73e08c5d609da165243f7
Daniele Silva <daniele.silva@edge.ufal.br>
2022-12-13 16:28:26 -0300

    Adicionar arquivos de projeto.

---
+++ OrientacaoObjetosComCSharp/Program.cs
@@ -0,0 +1,11 @@
+﻿using OrientacaoObjetosComCSharp;
+
+ContaBancaria conta_1 = new ContaBancaria();
+conta_1.titular = "André Silva";
+conta_1.numero_agencia = 15;
+conta_1.saldo = 125.50;
+conta_1.conta = "12356";
+conta_1.chavePix.cpf = "0125893454";
+
+Console.WriteLine($"saldo: {conta_1.saldo}");
+Console.ReadLine();
