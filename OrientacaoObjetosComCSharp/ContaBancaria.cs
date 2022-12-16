a128b3462815810f233a1a1981b347eb7a0ab522
Daniele Silva <daniele.silva@edge.ufal.br>
2022-12-16 13:34:00 -0300

    feat: criado metodo de deposito, saque e transferencia na classe ContaBancaria

--- OrientacaoObjetosComCSharp/ContaBancaria.cs
+++ OrientacaoObjetosComCSharp/ContaBancaria.cs
@@ -13,10 +13,32 @@ namespace OrientacaoObjetosComCSharp
         public string titular;
         public double saldo;
         public ChavePix chavePix;
-
-        public void DepositarSaldo(double valor)
+        public void depositarSaldo(double valor)
         {
             this.saldo += valor;
+        }
+
+        public double transferirSaldo(double valor, ContaBancaria destino)
+        {
+            this.saldo -= valor;
+            destino.saldo = valor;
+            Console.WriteLine($"Você transferiu {valor}, para a conta: {destino.conta}, seu saldo agora é: {saldo}");
+            return valor;
+        }
+
+        public void sacarSaldo(double valor)
+        {
+            if (valor > 0 && valor < this.saldo)
+            {
+
+                this.saldo -= valor;
+                Console.WriteLine($"Você sacou {valor} do seu saldo, restando {this.saldo}");
+            }
+            else
+            {
+                Console.WriteLine($"Você não tem saldo suficiente, seu saldo é: {this.saldo}");
+
+            }
         }
 
         //cadastro de cliente
@@ -27,20 +49,21 @@ namespace OrientacaoObjetosComCSharp
 
     }
 
-    internal class Cliente
+    internal class Cliente : ContaBancaria
     {
-        public int id;
-        public string nome;
-        public double renda;
-        public string profissão;
-        public Standard standard;
-        public Premium premium;
-        public Gold gold;
+         public int id;
+         public string nome;
+         public double renda;
+         public string profissao;
+         public bool is_standard;
+         public bool is_premium;
+         public bool is_gold;
         public void setClienteStandard()
         { 
         if (this.renda <= 4999.99)
         {
                 Standard.is_standard = true;
+                this.is_standard = true;
                 Standard.renda = this.renda;
                 //limite_transação = 1000
         }
@@ -50,7 +73,8 @@ namespace OrientacaoObjetosComCSharp
         {
             if (this.renda >= 18.000)
             {
-                Gold.is_gold = true; 
+                Gold.is_gold = true;
+                this.is_gold = true;
                 Gold.renda = this.renda;
                 //limite_transação = 5000
 
@@ -63,6 +87,7 @@ namespace OrientacaoObjetosComCSharp
             if (this.renda >= 5000 && this.renda <= 17.999)
             {
                 Premium.is_premium = true;
+                this.is_premium = true;
                 Premium.renda = this.renda;
                 //limite_transação = não tem
             }
--- OrientacaoObjetosComCSharp/Program.cs
+++ OrientacaoObjetosComCSharp/Program.cs
@@ -1,13 +1,33 @@
 ﻿using OrientacaoObjetosComCSharp;
 
 ContaBancaria conta_1 = new ContaBancaria();
-conta_1.titular = "André Silva";
+ContaBancaria conta_2 = new ContaBancaria();
+
 conta_1.numero_agencia = 15;
 conta_1.saldo = 125;
-conta_1.conta = "12356";
+conta_1.conta = "123567";
+conta_2.numero_agencia = 15;
+conta_2.saldo = 155;
+conta_2.conta = "conta_2";
+//conta_1.chavePix.cpf = "15154848";
+//conta_1.cadastroCliente.profissao = "professor";
+//conta_1.cadastroCliente.renda = 4000;
+//conta_1.cadastroCliente.setClienteGold();
+//conta_1.cadastroCliente.setClientePremium();
+//conta_1.cadastroCliente.setClienteStandard();
 
-Console.WriteLine(conta_1.saldo);
-conta_1.DepositarSaldo(50);
+conta_1.depositarSaldo(50);
 Console.WriteLine($"saldo: {conta_1.saldo}");
+conta_1.sacarSaldo(350);
+Console.WriteLine($"saldo: {conta_2.saldo}");
+conta_2.transferirSaldo(100, conta_1); 
+//conta_1.getTransferencia();
+
+
+
+//Console.WriteLine(conta_1.cadastroCliente.is_premium);
+//Console.WriteLine(conta_1.cadastroCliente.is_gold);
+//Console.WriteLine(conta_1.cadastroCliente.is_standard);
 
 Console.ReadLine();
+
