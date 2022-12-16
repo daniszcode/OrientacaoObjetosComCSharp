2ec2049cc6326d2c278a64ff3b3e6187fde0a743
Daniele Silva <daniele.silva@edge.ufal.br>
2022-12-16 13:33:22 -0300

    feat: criado metodo de transferencia de valores entre as classes, metodo de saque na classe contas bancaria e metodo de deposito

--- OrientacaoObjetosComCSharp/Program.cs
+++ OrientacaoObjetosComCSharp/Program.cs
@@ -6,7 +6,6 @@ conta_1.numero_agencia = 15;
 conta_1.saldo = 125;
 conta_1.conta = "12356";
 
-
 Console.WriteLine(conta_1.saldo);
 conta_1.DepositarSaldo(50);
 Console.WriteLine($"saldo: {conta_1.saldo}");
